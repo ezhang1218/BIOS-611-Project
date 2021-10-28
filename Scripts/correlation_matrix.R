a0210b24562eec2d6d5d9a67228608b1fc9fe1f2
Eric Zhang <ezhang1218@gmail.com>
2021-10-27 20:19:55 -0400

    png

--- Scripts/correlation_matrix.R
+++ Scripts/correlation_matrix.R
@@ -10,5 +10,10 @@ continuous_var <- bikes[,c(6,7,8,9)]
 cormat <- round(cor(continuous_var),2)
 melted <- melt(cormat)
 
-ggplot(data = melted, aes(x=Var1, y=Var2, fill=value)) + 
+
+p <- ggplot(data = melted, aes(x=Var1, y=Var2, fill=value)) + 
   geom_tile()
+
+ggsave("Figures/correlation_matrix.png",plot=p)
+
+p
